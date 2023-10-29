66a5c6cac4945ffdeed492c86adae22df0d3e502
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-10-29 12:22:23 +0100

    play with variables

--- 00. init/00. init/Program.cs
+++ 00. init/00. init/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string hello = "Hello";
+
+Console.WriteLine(hello + " World!");
+Console.WriteLine("Salut " + hello + "!");
 
