9b2d9b8fa208282b95824695ad55ab7c28327988
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-10-29 17:34:41 +0100

    add prompt, update doc + gitignore

--- 00. init/00. init/Program.cs
+++ 00. init/00. init/Program.cs
@@ -1,7 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+// See https://aka.ms/new-console-template for more information
 
-string hello = "Hello";
+System.Console.WriteLine("TEST");
 
-Console.WriteLine(hello + " World!");
-Console.WriteLine("Salut " + hello + "!");
+Console.OutputEncoding = System.Text.Encoding.UTF8;
 
+Console.WriteLine("what's your name ?");
+
+string name = Console.ReadLine();
+
+Console.WriteLine("Hello, I am " + name);
+Console.WriteLine(name + " is my name !");
---
+++ README.md
@@ -0,0 +1,14 @@
+# C#
+
+installer .net ([dotnet](https://dotnet.microsoft.com/en-us/download))
+
+sous mac :
+
+`brew install mono-libgdiplus mono`
+
+pour lancer le program nouvellement developé :
+
+```shell
+csc "00. init/00. init/Program.cs"
+mono "00. init/00. init/Program.exe"
+```
