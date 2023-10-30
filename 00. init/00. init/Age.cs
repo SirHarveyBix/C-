a6e98a75678ef2f9869dbec57e2d5fc3139217e6
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-10-30 11:28:52 +0100

    transform AGe.cs into a full C# object

--- 00. init/00. init/Age.cs
+++ 00. init/00. init/Age.cs
@@ -1,28 +1,40 @@
-﻿using System;
+﻿// using System;
 // See https://aka.ms/new-console-template for more information
 
-Console.OutputEncoding = System.Text.Encoding.UTF8;
-
-// ask name in prompt
-Console.Write("what's your name ? ");
-string name = Console.ReadLine();
-
-bool isValidAge = false;
-
-while (!isValidAge)
+namespace Age
 {
-Console.Write("How old are you ? ");
-// ask age in prompte throw error if string
-int age = 0;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-    try
-    {
-        age = int.Parse(Console.ReadLine());
-        isValidAge = true;
-        Console.WriteLine("Hello, " + name + " you are " + age + " year's old.");
-    }
-    catch (FormatException)
-    {
-        Console.WriteLine("your age is not a valid number..");
+            // ask name in prompt
+            Console.Write("what's your name ? ");
+            string name = Console.ReadLine();
+
+            int age = 0;
+
+            while (age <= 0)
+            {
+                Console.Write("How old are you ? ");
+                // ask age in prompte throw error if string
+
+                try
+                {
+                    age = int.Parse(Console.ReadLine());
+
+                    if (age <= 0)
+                    {
+                        Console.WriteLine("your age is not a valid number.");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("your age is not a valid number..");
+                }
+            }
+            Console.WriteLine("Hello, " + name + " you are " + age + " year's old.");
+        }
     }
 }
