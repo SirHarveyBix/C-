e32db6c04b6581fb6c17949a7bdb60371cb4bb02
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-10-30 14:38:28 +0100

    some tests

---
+++ 00. init/00. init/OldProgram.cs
@@ -0,0 +1,11 @@
+﻿// using System;
+// // See https://aka.ms/new-console-template for more information
+
+// Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+// Console.Write("what's your name ?");
+
+// string name = Console.ReadLine();
+
+// Console.WriteLine("Hello, I am " + name);
+// Console.WriteLine(name + " is my name !");
--- 00. init/00. init/Program.cs
+++ 00. init/00. init/Program.cs
@@ -1,11 +1,42 @@
 ﻿using System;
+
 // See https://aka.ms/new-console-template for more information
 
-Console.OutputEncoding = System.Text.Encoding.UTF8;
+namespace Age_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-Console.Write("what's your name ?");
+            // ask name in prompt
+            Console.Write("what's your name ? ");
+            string name = Console.ReadLine();
 
-string name = Console.ReadLine();
+            int age = 0;
 
-Console.WriteLine("Hello, I am " + name);
-Console.WriteLine(name + " is my name !");
+            while (age <= 0)
+            {
+                Console.Write("How old are you ? ");
+                // ask age in prompt throw error if string
+
+                try
+                {
+                    string ag_str = Console.ReadLine();
+                    age = int.Parse(ag_str);
+
+                    if (age <= 0)
+                    {
+                        Console.WriteLine("your age is not a valid number.");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("your age is not a valid number..");
+                }
+            }
+            Console.WriteLine("Hello, " + name + " you are " + age + " year's old.");
+        }
+    }
+}
