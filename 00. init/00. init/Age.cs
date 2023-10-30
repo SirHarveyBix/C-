d3fff360984893860a3c8adee0982bc205e0c974
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-10-30 16:27:10 +0100

    refactor to function : AskAge()

--- 00. init/00. init/Age.cs
+++ 00. init/00. init/Age.cs
@@ -5,6 +5,30 @@ namespace Age
 {
     class Program
     {
+        static int AskAge()
+        {
+            int age = 0;
+            while (age <= 0)
+            {
+                Console.Write("How old are you ? ");
+                try
+                {
+                    string ag_str = Console.ReadLine();
+                    age = int.Parse(ag_str);
+
+                    if (age <= 0)
+                    {
+                        Console.WriteLine("your age is not a valid number.");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("your age is not a valid number..");
+                }
+            }
+            return age;
+        }
+
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
@@ -23,27 +47,8 @@ namespace Age
                     System.Console.WriteLine("Your name cannot be empty!");
                 }
             }
+            int age = AskAge();
 
-            int age = 0;
-            while (age <= 0)
-            {
-                Console.Write("How old are you ? ");
-                // ask age in prompt throw error if string
-                try
-                {
-                    string ag_str = Console.ReadLine();
-                    age = int.Parse(ag_str);
-
-                    if (age <= 0)
-                    {
-                        Console.WriteLine("your age is not a valid number.");
-                    }
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("your age is not a valid number..");
-                }
-            }
             Console.WriteLine("Hello, " + name + " you are " + age + " year's old.");
         }
     }
