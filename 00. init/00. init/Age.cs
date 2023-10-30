b73208f1c5d386c6bdd5a83010d91559375b61fe
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-10-30 10:47:32 +0100

    cleanup + add prompt for age + small error management

---
+++ 00. init/00. init/Age.cs
@@ -0,0 +1,21 @@
+﻿using System;
+// See https://aka.ms/new-console-template for more information
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+// ask name in prompt
+Console.Write("what's your name ? ");
+string name = Console.ReadLine();
+
+Console.Write("How old are you ? ");
+// ask age in prompte throw error if string
+try
+{ 
+    int age = Int32.Parse(Console.ReadLine());
+
+    Console.WriteLine("Hello, " + name + " you are " + age + " year's old.");
+}
+catch (FormatException)
+{
+    Console.Write("your age is not a valid number..");
+}
--- 00. init/00. init/Program.cs
+++ 00. init/00. init/Program.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 // See https://aka.ms/new-console-template for more information
 
-System.Console.WriteLine("TEST");
-
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-Console.WriteLine("what's your name ?");
+Console.Write("what's your name ?");
 
 string name = Console.ReadLine();
 
