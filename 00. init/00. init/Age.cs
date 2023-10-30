feb7f09c0dafdcde4298680c8c0120054d1adfc9
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-10-30 16:02:00 +0100

    better config (00. init.csproj) + rename properly files + add validation (.Trim()) on name

---
+++ 00. init/00. init/Age.cs
@@ -0,0 +1,50 @@
+﻿using System;
+// See https://aka.ms/new-console-template for more information
+
+namespace Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            string name = "";
+
+            while (name.Trim() == "")
+            {
+                // ask name in prompt
+                Console.Write("what's your name ? ");
+                name = Console.ReadLine().Trim();
+                name = name.Trim();
+
+                if (name.Trim() == "")
+                {
+                    System.Console.WriteLine("Your name cannot be empty!");
+                }
+            }
+
+            int age = 0;
+            while (age <= 0)
+            {
+                Console.Write("How old are you ? ");
+                // ask age in prompt throw error if string
+                try
+                {
+                    string ag_str = Console.ReadLine();
+                    age = int.Parse(ag_str);
+
+                    if (age <= 0)
+                    {
+                        Console.WriteLine("your age is not a valid number.");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("your age is not a valid number..");
+                }
+            }
+            Console.WriteLine("Hello, " + name + " you are " + age + " year's old.");
+        }
+    }
+}
--- 00. init/00. init/Program.cs
+++ 00. init/00. init/Program.cs
@@ -1,42 +1,11 @@
 ﻿using System;
-
 // See https://aka.ms/new-console-template for more information
 
-namespace Age_Program
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            // ask name in prompt
-            Console.Write("what's your name ? ");
-            string name = Console.ReadLine();
+Console.Write("what's your name ?");
 
-            int age = 0;
+string name = Console.ReadLine();
 
-            while (age <= 0)
-            {
-                Console.Write("How old are you ? ");
-                // ask age in prompt throw error if string
-
-                try
-                {
-                    string ag_str = Console.ReadLine();
-                    age = int.Parse(ag_str);
-
-                    if (age <= 0)
-                    {
-                        Console.WriteLine("your age is not a valid number.");
-                    }
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("your age is not a valid number..");
-                }
-            }
-            Console.WriteLine("Hello, " + name + " you are " + age + " year's old.");
-        }
-    }
-}
+Console.WriteLine("Hello, I am " + name);
+Console.WriteLine(name + " is my name !");
