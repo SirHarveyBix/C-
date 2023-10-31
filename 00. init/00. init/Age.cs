b00793ba0d0cdb15a415691766e379394efffdf5
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-10-31 15:04:43 +0100

    smalls cleanup + comments

--- 00. init/00. init/Age.cs
+++ 00. init/00. init/Age.cs
@@ -8,17 +8,18 @@ namespace Age
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            //// Ask for name(s)
-            //string nameFirst = AskName(1);
-            //string nameSecond = AskName(2);
+            /*
+            // Ask for name(s)
+            string nameFirst = AskName(1);
+            string nameSecond = AskName(2);
 
-            //// Ask for age(s)
-            //int ageFirst = AskAge(nameFirst);
-            //int ageSecond = AskAge(nameSecond);
+            // Ask for age(s)
+            int ageFirst = AskAge(nameFirst);
+            int ageSecond = AskAge(nameSecond);
 
-
-            //OutputData(nameFirst, ageFirst);
-            //OutputData(nameSecond, ageSecond);
+            OutputData(nameFirst, ageFirst);
+            OutputData(nameSecond, ageSecond);
+            */
             const int ASKING_TIME = 3;
 
             for (int i = 1; i <= ASKING_TIME; i++)
@@ -32,8 +33,7 @@ namespace Age
 
         static void OutputData(string name, int age, float size = 0) // parametre facultatif 
         {
-            Console.WriteLine();
-            Console.WriteLine($"Hello, {name} you are {age} year's old.");
+            Console.WriteLine($"\nHello, {name} you are {age} year's old,");
             Console.WriteLine($"You'll be soon {(age + 1)} !");
 
             bool isGrownUpEnough = age > 18;
@@ -54,7 +54,6 @@ namespace Age
             {
                 Console.WriteLine($"You are {size}m tall!");
             }
-            Console.WriteLine();
         }
 
         static int AskAge(string name)
@@ -65,6 +64,7 @@ namespace Age
                 Console.Write($"{name}, How old are you ? ");
                 try
                 {
+                    // ask age in prompt
                     string age_str = Console.ReadLine();
                     age = int.Parse(age_str);
 
@@ -87,8 +87,8 @@ namespace Age
 
             while (name.Trim() == "")
             {
-                // ask name in prompt
                 Console.Write("what's your name ? ");
+                // ask name in prompt & cleanup space around
                 name = Console.ReadLine().Trim();
 
                 if (name == "")
