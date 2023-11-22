29fdab51f5c3c5d596209f2654ae90cde6d77843
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-11-22 18:23:21 +0100

    init methods (void, int) + add some !

--- 02.function_methods/methods/Program.cs
+++ 02.function_methods/methods/Program.cs
@@ -4,7 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        WriteSomething();
+
+        WriteSomethidSpecific("lol\n");
+
+        Console.WriteLine(Add(1, 2));
+        Console.WriteLine(Add(Add(1, 2), Add(1, 2)));
+
+        Console.WriteLine(Multiply(4, 2));
+        Console.WriteLine(Divide(4, 2));
+    }
+
+    // access modifier [static], return type [void], method name, parameter (type name).
+    public static void WriteSomething()
+    {
+        Console.WriteLine("Inside a method.\n");
+    }
+
+    public static void WriteSomethidSpecific(string text)
+    {
+        Console.WriteLine("arg : " + text);
+    }
+
+    public static int Add(int number1, int number2)
+    {
+        return number1 + number2;
+    }
+
+    public static int Multiply(int number1, int number2)
+    {
+        return number1 * number2;
+    }
+
+    public static double Divide(int number1, int number2)
+    {
+        return number1 / number2;
     }
 }
-
