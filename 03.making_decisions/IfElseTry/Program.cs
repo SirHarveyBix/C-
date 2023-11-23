5e6c845e55093234739f5081b208d5cfcb996b72
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-11-23 18:42:10 +0100

    playing with elseif blocks

--- 03.making_decisions/IfElseTry/Program.cs
+++ 03.making_decisions/IfElseTry/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace IfElseTry;
+﻿using System;
 
+namespace IfElseTry;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("what temperature is it outisde ?");
+
+        string temperature = Console.ReadLine();
+        int temperatureInt = int.Parse(temperature);
+
+        if (temperatureInt < 20)
+        {
+            Console.WriteLine("Take the coat");
+        }
+        else if (temperatureInt == 20)
+        {
+            Console.WriteLine("Pants and Pull Over are fine");
+        }
+        else if (temperatureInt > 30)
+        {
+            Console.WriteLine("Supa dupa hot!");
+        }
+        else
+        {
+            Console.WriteLine("you'll be a better judge than a stupid program");
+        }
     }
 }
 
