9496311fb4226def50df39a5c3c5c773ae316689
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-11-24 17:33:09 +0100

    add small exercise

--- 03.making_decisions/NestedIfs/Program.cs
+++ 03.making_decisions/NestedIfs/Program.cs
@@ -1,13 +1,17 @@
-﻿namespace NestedIfs;
+﻿using System;
 
+namespace NestedIfs;
 class Program
 {
+    static string username;
+    static string password;
+
     static void Main(string[] args)
     {
         bool isAdmin = false;
         bool isRegistered = true;
 
-        Console.WriteLine("Pleas enter your username : ");
+        Console.WriteLine("Please enter your username : ");
         string userName = "";
         userName = Console.ReadLine();
 
@@ -20,7 +24,6 @@ class Program
             //if (userName.Equals("admin"))
             //{
             Console.WriteLine("Hello there, Admin");
-
             //        }
             //    }
 
@@ -29,6 +32,41 @@ class Program
         {
             Console.WriteLine("Hello there, You are registered");
         }
+
+        Register();
+        Login();
         Console.Read();
+    }
+
+    public static void Register()
+    {
+        Console.WriteLine("Please enter your username : ");
+        username = Console.ReadLine();
+
+        Console.WriteLine("Please enter your password : ");
+        password = Console.ReadLine();
+
+        Console.WriteLine("Registration Completed\n-- -- -- -- -- -- -- -- --");
+    }
+
+    public static void Login()
+    {
+        Console.WriteLine("Please enter your username : ");
+        if (username == Console.ReadLine())
+        {
+            Console.WriteLine("Please enter your password : ");
+            if (password == Console.ReadLine())
+            {
+                Console.WriteLine("Login successful");
+            }
+            else
+            {
+                Console.WriteLine("Login failed, wrong password. Restart program");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Login failed, wrong username. Restart program");
+        }
     }
 }
