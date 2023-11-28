f9212a17dba1bb304a2bc367b714808db8f4fb99
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-11-28 11:56:51 +0100

    Creating our frst class

---
+++ 05.OOP/FirstClass/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FirstClass
+{
+    internal class Car
+    {
+        public Car(string brand) // Constructor
+        {
+            Console.WriteLine(brand + " has been created.");
+
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine("Car is Driving.");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Car Stopped.");
+        }
+    }
+}
