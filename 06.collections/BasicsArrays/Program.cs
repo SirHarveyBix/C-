a057343a9ce1c2203e9fc4e616922e0cd67c2424
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-11-29 14:42:26 +0100

    declare and initialize array

--- 06.collections/BasicsArrays/Program.cs
+++ 06.collections/BasicsArrays/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace BasicsArrays;
+﻿using System;
 
+namespace BasicsArrays;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] grades = new int[5];
+
+        grades[0] = 20;
+        grades[1] = 50;
+
+        Console.WriteLine("grade at index 0 '[0]' : {0}\n", grades[0]);
+
+        Console.WriteLine("write a number");
+        string input = Console.ReadLine();
+
+        grades[1] = int.Parse(input);
+        Console.WriteLine("grade at index 1 '[1]' : {0}\n", grades[1]);
+
+        int[] gradeStudentA = { 20, 13, 18, 19, 2 };
+        int[] gradeStudentB = new int[] { 15, 6, 8, 19, 20 };
+
+        Console.WriteLine("gradeStudentB.Length" + gradeStudentB.Length);
     }
 }
-
