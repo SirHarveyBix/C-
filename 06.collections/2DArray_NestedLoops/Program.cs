b810cddbc86bf848017fd8040fa3d434e2e1a9a9
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-11-30 12:42:43 +0100

    add loop for display : even numbers,  diagonal numbers & reverse d iagonal numbers

--- 06.collections/2DArray_NestedLoops/Program.cs
+++ 06.collections/2DArray_NestedLoops/Program.cs
@@ -1,5 +1,6 @@
-﻿namespace _2DArray_NestedLoops;
+﻿using System;
 
+namespace _2DArray_NestedLoops;
 class Program
 {
     static int[,] matrix =
@@ -22,19 +23,66 @@ class Program
         Console.WriteLine("\n\n2D array: matrix - using nested for loop:");
         // Nested for loop
 
-        // outer for loop :
+        // outter for loop :
         for (int i = 0; i < matrix.GetLength(0); i++)
         {
             // inner for loop :
             for (int j = 0; j < matrix.GetLength(1); j++)
             {
                 Console.Write(matrix[i, j] + ", ");
-                // thnaks to th nested fo loop, adding logic is possible :
-               // matrix[i, j] = 0;
+                // thanks to the nested for loop, adding logic is now possible
             }
         }
 
-        Console.WriteLine("\n\n'matrix.GetLength(0)' - matrix[,] has '{0}' rows ", matrix.GetLength(0));
-        Console.WriteLine("'matrix.GetLength(1)' - each matrix[,] rows has '{0}' key ", matrix.GetLength(1));
+        Console.WriteLine("\n\n'matrix.GetLength(0)' - matrix[,] has '{0}' rows", matrix.GetLength(0));
+        Console.WriteLine("'matrix.GetLength(1)' - each matrix[,] rows has '{0}' key\n", matrix.GetLength(1));
+
+        evenNumbers();
+        diagonalNumbers();
+        reverseDiagonalNumbers();
+    }
+
+    static void evenNumbers()
+    {
+        Console.WriteLine("\nEven numbers :");
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (matrix[i, j] % 2 == 0)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void diagonalNumbers()
+    {
+        Console.WriteLine("\nDiagonal numbers :");
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            if (i < matrix.GetLength(1)) // check if 'i' stays in limit positions
+            {
+                Console.WriteLine(matrix[i, i]);
+            }
+        }
+    }
+
+    static void reverseDiagonalNumbers()
+    {
+        Console.WriteLine("\nReverse Diagonal numbers :");
+
+        for (int i = 0, j = 2; i < matrix.GetLength(0) & j >= 0; i++, j--)
+        {
+            Console.WriteLine(matrix[i, j]);
+        }
     }
 }
