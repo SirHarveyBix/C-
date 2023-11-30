029db55c1850b3630bd3e9819e2ae1269ed5efdf
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-11-30 18:28:48 +0100

    compare jagged array to multi dimensional arrays + comments

--- 06.collections/JaggedArrays/Program.cs
+++ 06.collections/JaggedArrays/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Reflection;
 
 namespace JaggedArrays;
 class Program
@@ -35,18 +34,61 @@ class Program
                 Console.Write("{0}, ", jaggedArray2[i][j]);
             }
         }
-
-        //      index               0   1   2   3  
-        // normal array type int : [15, 21, 23, 13]
-        // jagged array type int :
-        // [
-        //   [ 2, 3, 5, 7, 11 ],
-        //   [ 1, 2, 3 ]
-        // ];
+        /*  Visual representation : 
+                index            0   1   2   3  
+        normal array type int : [15, 21, 23, 13]
+        jagged array type int :
+        [
+            [ 2, 3, 5, 7, 11 ],
+            [ 1, 2, 3 ]
+        ]; */
 
         // decalre normal array
         int[] regularArray = new int[4] { 15, 21, 23, 13 }; // [4] indicate the maxlength of the array
 
         Console.WriteLine("\n\nregularArray[0] : '{0}'", regularArray[0]);
+
+        // decalre and initialize a jagged array
+        // use jagged array when complex data structure, with elements with differrents sizes
+        int[][] triangle = new int[][]
+        {
+            new int[] { 1 },
+            new int[] { 2, 3 },
+            new int[] { 4, 5, 6 },
+            new int[] { 7, 8, 9, 10 }
+        };
+        Console.WriteLine("\njagged array - for each loop: ");
+
+        //'foreach' loops, print each row:
+        foreach (int[] row in triangle)
+        {
+            Console.WriteLine();
+            foreach (int number in row)
+            {
+                Console.Write(" " + number);
+            }
+        }
+
+        // decalre and initialize a 2D array
+        // use multi dimensional array for mathematical operation on the grid of number of same sizes
+        int[,] grid = new int[,]
+        {
+            { 1 ,2 },
+            { 2, 4 },
+            { 5, 6 },
+            { 7, 8 },
+            { 9, 10 }
+        };
+        Console.WriteLine("\n\n2D array - for loop:");
+
+        //'for' loops, print each number in the grid:
+        for (int i = 0; i < grid.GetLength(0); i++)
+        {
+            Console.WriteLine();
+            for (int j = 0; j < grid.GetLength(1); j++)
+            {
+                Console.Write(" " + grid[i, j]);
+            }
+        }
     }
 }
