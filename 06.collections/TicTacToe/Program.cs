e01f896b8fd53fee6110cec9049a199d132500ed
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-11-30 13:32:42 +0100

    init TicTacToe playground

---
+++ 06.collections/TicTacToe/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TicTacToe;
+class Program
+{
+    static char[,] playField =
+    {
+         { '1','2','3' }, // row 0
+         { '4','5','6' }, // row 1
+         { '7','8','9' }  // row 2
+    };
+
+    static void Main(string[] args)
+    {
+        int player = 2;
+        int input = 0;
+        bool inputCorrect = true;
+
+        SetField();
+    }
+
+    public static void SetField()
+    {
+
+        Console.Clear();
+        Console.WriteLine("     |     |      ");
+        Console.WriteLine("  {0}  |  {1}  |  {2}", playField[0, 0], playField[0, 1], playField[0, 2]);
+        Console.WriteLine("_____|_____|_____");
+        Console.WriteLine("     |     |     ");
+        Console.WriteLine("  {0}  |  {1}  |  {2}", playField[1, 0], playField[1, 1], playField[1, 2]);
+        Console.WriteLine("_____|_____|_____");
+        Console.WriteLine("     |     |     ");
+        Console.WriteLine("  {0}  |  {1}  |  {2}", playField[2, 0], playField[2, 1], playField[2, 2]);
+        Console.WriteLine("     |     |     ");
+        //turns++;
+
+    }
+}
