5364d0c70d501fb0fb4290cca4af12fb47324ee4
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-12-01 13:07:16 +0100

    playing with 'params' keyword

--- 06.collections/ArraysAsParams/Program.cs
+++ 06.collections/ArraysAsParams/Program.cs
@@ -17,7 +17,36 @@ class Program
         int[] happiness = { 2, 3, 4, 5, 6 };
         SunIsShining(happiness);
         foreach (int y in happiness) { Console.WriteLine(y); }
+
+        Console.WriteLine("\n # # # # # # \n");
+
+        ParamsMethod("this", "is", "a", "long", "string", "blah", "bla", "and", "END!");
+
+        Console.WriteLine("\n # # # # # # \n");
+
+        int price = 50;
+        float pi = 3.14f;
+        char at = '@';
+        string book = "Lord of the ring.";
+
+        // 'params' keyword, with 'object[]' type,
+        // allow to pass as many params, and any type we want.
+
+        ParamsMethodStuff(price, pi, at, book);
+        ParamsMethodStuff("Hello", 5.3, '£');
     }
+
+    public static void ParamsMethod(params string[] sentence) // 'params': as many params as we want
+    {
+        for (int i = 0; i < sentence.Length; i++) { Console.Write(sentence[i] + " "); }
+    }
+
+    public static void ParamsMethodStuff(params object[] stuff)
+    {
+        foreach (object thing in stuff) { Console.Write(thing + " "); }
+        Console.WriteLine("\n# # # #\n");
+    }
+
 
     static double GetAverage(int[] gradesArray)
     {
