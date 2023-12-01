dd0de5ecd19e27119891b3a71b13306fc9e60281
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2023-12-01 16:42:00 +0100

    add small exercise: List<int>

--- 06.collections/ArrayLists/Program.cs
+++ 06.collections/ArrayLists/Program.cs
@@ -4,11 +4,12 @@ using System.Collections;
 namespace ArrayLists;
 class Program
 {
+    static List<int> numberList = new List<int>();
+
     static void Main(string[] args)
     {
         // Declaring an ArrayList
         ArrayList firstArrayList = new ArrayList(); // undefined amount of object
-        ArrayList secondArrayList = new ArrayList(100); // '100' is amount of object whithin the ArrayList
 
         firstArrayList.AddRange(new[] { 23, 25.89, 15 }); // [test:{ adding many values
         firstArrayList.Add("Kevain");
@@ -34,5 +35,19 @@ class Program
         // Declaring a List - type casting is mandatory: <string>
         List<string> firstList = new List<string>();
         List<int> secondtList = new List<int>(5) { 1, 2, 3, 4, 5 }; // <type>, (amount) of values, {value}
+
+        Solution();
+    }
+
+    // Write a method that returns a list of even integer numbers between 100 and 170 (inclusive).
+    public static List<int> Solution()
+    {
+        for (int i = 100; i <= 170; i++)
+        {
+            if (i % 2 == 0) numberList.Add(i);
+        }
+        Console.WriteLine("Last: " + numberList.Last());
+        Console.WriteLine("First: " + numberList.First());
+        return numberList;
     }
 }
